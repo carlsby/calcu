90a78331a1f641ca80f192f233be03336ee00f7b
Carlsby <carlandersson99@outlook.com>
2025-02-16 00:42:06 +0100

    Update Remove top menu

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Calcu',
         width: 300,
-        height: 470,
+        height: 430,
         resizable: false,
         icon: path.join(__dirname, './renderer/assets/c.png') 
     });
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+    mainWindow.removeMenu();
 }
 
 app.whenReady().then(() => {
