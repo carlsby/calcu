51fe0f212dd719f4f0b8dde5144b313fe3e2a1fc
Carlsby <carlandersson99@outlook.com>
2025-02-15 23:19:02 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Calcu',
+        width: 300,
+        height: 470,
+        resizable: false
+    });
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+});
+
+try {
+    require('electron-reloader')(module)
+  } catch (_) {}
